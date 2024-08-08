383280baa8c8902a80f6a094fad49047c99c2261
kurushimee <jermacoff@gmail.com>
2024-08-08 22:08:11 +0700

    Add ability to close inspection menu

--- code/Components/UI/Inspection.cs
+++ code/Components/UI/Inspection.cs
@@ -1,6 +1,17 @@
 public sealed class Inspection : Component {
 	[Property] private ModelRenderer InspectionModel { get; set; }
 
+	protected override void OnUpdate() {
+		if (GameManager.Instance.State != PlayerState.inspecting) return;
+
+		if (Input.EscapePressed) CloseMenu();
+	}
+
+	private void CloseMenu() {
+		GameManager.Instance.State = PlayerState.walking;
+		GameObject.Enabled = false;
+	}
+
 	public void SetModel(Model model) {
 		InspectionModel.Model = model;
 	}
